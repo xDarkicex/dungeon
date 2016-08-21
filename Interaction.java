587266144434383e3cfcdd3392bce63e98b51dff
Daniel Caaz <daniel@caaz.me>
2016-08-21 11:25:51 -0700

    Interaction changes!

--- Interaction.java
+++ Interaction.java
@@ -2,19 +2,16 @@ import java.io.Console;
 public class Interaction {
   private static Console con = System.console();
   public static int choose(String[] options) {
+    int input = 0;
     String option_list = "";
-    // int i = 1;
     for(int i = 0; i<options.length; i++) { option_list += "["+(i+1)+" "+options[i]+"] "; }
-    int input = -1;
-    while(input == -1){
+    while(true) {
       try {
         Writer.yellow(option_list);
         input = Integer.parseInt(con.readLine());
-      } catch(NumberFormatException e) { Writer.red("Please input a valid number"); continue; }
-      if((input < 1) || (input > options.length)) {
-        Writer.red("Invalid input! 1-"+options.length+" please.");
-        input = -1; continue;
-      }
+        break;
+      } catch(NumberFormatException e) { Writer.red("Please input a valid number"); }
+      if((input < 1) || (input > options.length)) { Writer.red("Invalid input! 1-"+options.length+" please."); }
     }
     return input;
   }
--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-rm -f *.class && rm -f *.jar && javac Dungeon.java && jar cvfe Dungeon.jar Dungeon *.class && rm -f *.class
+rm -f *.class && rm -f *.jar && javac Dungeon.java && jar cvfe Dungeon.jar Dungeon *.class
+rm -f *.class && rm -f *.jar
